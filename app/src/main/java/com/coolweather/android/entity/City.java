87019d3cffa01e6faa5210407bbcef5b6b13de94
PinkGuy <pinkguy233@icloud.com>
2020-04-18 11:36:05 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/coolweather/android/entity/City.java
@@ -0,0 +1,58 @@
+package com.coolweather.android.entity;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * 市
+ */
+public class City extends DataSupport {
+
+    private int id;
+    private  String cityName;
+    private int cityCode;
+    private int provinceId;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+
+    public City() {
+
+    }
+
+    public City(int id, String cityName, int cityCode, int provinceId) {
+        this.id = id;
+        this.cityName = cityName;
+        this.cityCode = cityCode;
+        this.provinceId = provinceId;
+    }
+}
---
+++ app/src/main/java/com/coolweather/android/entity/Province.java
@@ -0,0 +1,45 @@
+package com.coolweather.android.entity;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * 省
+ */
+public class Province  extends DataSupport {
+    private int id;
+    private  String provinceName;
+    private  int provinceCode;
+
+    public Province() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public Province(int id, String provinceName, int provinceCode) {
+        this.id = id;
+        this.provinceName = provinceName;
+        this.provinceCode = provinceCode;
+    }
+}
